1d863ef1ec17e97cd338e0425d7727aa9e822157
Jesus-QC <jesusquirocampos@gmail.com>
2023-07-27 12:33:07 +0200

    Version Bump

--- HiddenTutorials/EntryPoint.cs
+++ HiddenTutorials/EntryPoint.cs
@@ -15,7 +15,7 @@ namespace HiddenTutorials;
 
 public class EntryPoint
 {
-	public const string Version = "1.0.0.0";
+	public const string Version = "1.0.0.1";
 	
 	[PluginAPI.Core.Attributes.PluginConfig] public static PluginConfig Config;
 
