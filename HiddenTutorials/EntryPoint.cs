6315b565057dfe2b9fa37feb06420a0411794c8c
jesusqc <jesusquirocampos@gmail.com>
2023-12-07 00:38:57 +0100

    🎅

--- HiddenTutorials/EntryPoint.cs
+++ HiddenTutorials/EntryPoint.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
+using HarmonyLib;
 using MEC;
 using Mirror;
 using PlayerRoles;
@@ -10,15 +9,19 @@ using PluginAPI.Core.Attributes;
 using PluginAPI.Enums;
 using PluginAPI.Events;
 using RelativePositioning;
+using UnityEngine;
 
 namespace HiddenTutorials;
 
+// Thanks to xNexusACS for helping to test!
 public class EntryPoint
 {
-	public const string Version = "1.0.0.1";
+	public const string Version = "1.0.0.2";
 	
 	[PluginAPI.Core.Attributes.PluginConfig] public static PluginConfig Config;
 
+	private Harmony _harmony = new Harmony("com.jesusqc.hiddentutorials");
+	
 	[PluginEntryPoint("HiddenTutorials", Version, "Makes tutorials hidden to spectators", "Jesus-QC")]
 	private void Init()
 	{
@@ -27,70 +30,60 @@ public class EntryPoint
         
 		Log.Raw($"<color=blue>Loading HiddenTutorials {Version} by Jesus-QC</color>");
 		EventManager.RegisterEvents(this);
+		
+		_harmony.PatchAll();
 	}
 	
-	[PluginEvent(ServerEventType.PlayerChangeRole)]
-	private void OnPlayerChangeRole(PlayerChangeRoleEvent ev)
+	[PluginEvent(ServerEventType.PlayerChangeSpectator)]
+	private void OnPlayerChangeSpectator(PlayerChangeSpectatorEvent ev)
 	{
-		switch (ev.NewRole)
-		{
-			case RoleTypeId.Tutorial:
-				Timing.CallDelayed(0.1f, () =>
-				{
-					ChangeAppearance(ev.Player, RoleTypeId.Filmmaker, Player.GetPlayers().Where(ply => ply.Role is RoleTypeId.Spectator && ply != ev.Player));
-				});
-				break;
-
-			case RoleTypeId.Spectator:
-				Timing.CallDelayed(0.1f, () =>
-				{
-					foreach (Player ply in Player.GetPlayers().Where(ply => ply.Role is RoleTypeId.Tutorial && ply != ev.Player))
-						ChangeAppearance(ply, RoleTypeId.Filmmaker, new []{ev.Player});
-				});
-				return;
-		}
+		if (IsWhitelisted(ev.Player.ReferenceHub))
+			return;
 		
-		if (ev.OldRole.RoleTypeId is not RoleTypeId.Spectator)
+		if (ev.OldTarget?.Role is RoleTypeId.Tutorial)
+			ResyncSpectator(ev.Player, ev.OldTarget);
+		
+		if (ev.NewTarget?.Role is not RoleTypeId.Tutorial)
 			return;
 
-		foreach (Player ply in Player.GetPlayers())
-		{
-			if (ply.Role is not RoleTypeId.Tutorial || ply == ev.Player)
-				continue;
-			
-			ChangeAppearance(ply, RoleTypeId.Tutorial, new []{ev.Player});
-		}
+		Timing.RunCoroutine(DesyncSpectator(ev.Player, ev.NewTarget));
 	}
 	
-	// Method grabbed from exiled
+	// Contains code from:
 	// -----------------------------------------------------------------------
 	// <copyright file="MirrorExtensions.cs" company="Exiled Team">
 	// Copyright (c) Exiled Team. All rights reserved.
 	// Licensed under the CC BY-SA 3.0 license.
 	// </copyright>
 	// -----------------------------------------------------------------------
-	private static void ChangeAppearance(Player player, RoleTypeId type, IEnumerable<Player> playersToAffect, byte unitId = 0)
+	private static void SendRoleAndPosition(Player spectator, Player target, RoleTypeId role, Vector3 position)
 	{
-		if (player.Role is RoleTypeId.Spectator or RoleTypeId.Filmmaker or RoleTypeId.Overwatch)
-			throw new InvalidOperationException("You cannot change a spectator into non-spectator via change appearance.");
-
 		NetworkWriterPooled writer = NetworkWriterPool.Get();
 		writer.WriteUShort(38952);
-		writer.WriteUInt(player.NetworkId);
-		writer.WriteRoleType(type);
-		if (type.GetTeam() == Team.FoundationForces)
-			writer.WriteByte(unitId);
+		writer.WriteUInt(target.NetworkId);
+		writer.WriteRoleType(role);
 
-		if (type != RoleTypeId.Spectator && player.RoleBase is IFpcRole fpc)
-		{
-			fpc.FpcModule.MouseLook.GetSyncValues(0, out ushort syncH, out _);
-			writer.WriteRelativePosition(new RelativePosition(player.ReferenceHub.transform.position));
-			writer.WriteUShort(syncH);
-		}
-
-		foreach (Player target in playersToAffect)
-			target.Connection.Send(writer.ToArraySegment());
+		if (target.RoleBase is not IFpcRole fpc)
+			return;
 		
+		fpc.FpcModule.MouseLook.GetSyncValues(0, out ushort syncH, out _);
+		writer.WriteRelativePosition(new RelativePosition(position));
+		writer.WriteUShort(syncH);
+		
+		spectator.Connection.Send(writer.ToArraySegment());
 		NetworkWriterPool.Return(writer);
 	}
+	
+	private static void ResyncSpectator(Player spectator, Player target)
+	{
+		SendRoleAndPosition(spectator, target, target.Role, Vector3.zero);
+	}
+
+	private static IEnumerator<float> DesyncSpectator(Player spectator, Player target)
+	{
+		yield return Timing.WaitForOneFrame;
+		SendRoleAndPosition(spectator, target, RoleTypeId.Spectator, Vector3.zero);
+	}
+
+	public static bool IsWhitelisted(ReferenceHub referenceHub) => referenceHub.authManager.RemoteAdminGlobalAccess || referenceHub.serverRoles.RemoteAdmin;
 }
---
+++ HiddenTutorials/Patches/PositionSyncingPatch.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using HarmonyLib;
+using NorthwoodLib.Pools;
+using PlayerRoles;
+using PlayerRoles.FirstPersonControl.NetworkMessages;
+using PlayerRoles.Spectating;
+using UnityEngine;
+
+namespace HiddenTutorials.Patches;
+
+[HarmonyPatch(typeof(FpcServerPositionDistributor), nameof(FpcServerPositionDistributor.GetNewSyncData))]
+public class PositionSyncingPatch
+{
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+    {
+        List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
+
+        int index = newInstructions.FindIndex(x => x.opcode == OpCodes.Newobj);
+        
+        newInstructions.InsertRange(index, new CodeInstruction[]
+        {
+            new (OpCodes.Ldarg_1),
+            new (OpCodes.Ldarg_0),
+            new (OpCodes.Call, AccessTools.Method(typeof(PositionSyncingPatch), nameof(CheckTutorialPosition))),
+        });
+        
+        foreach (CodeInstruction instruction in newInstructions)
+            yield return instruction;
+
+        ListPool<CodeInstruction>.Shared.Return(newInstructions);
+    }
+    
+    private static Vector3 CheckTutorialPosition(Vector3 position, ReferenceHub tutorial, ReferenceHub spectator)
+    {
+        if (EntryPoint.IsWhitelisted(spectator))
+            return position;
+        
+        if (tutorial.roleManager.CurrentRole.RoleTypeId != RoleTypeId.Tutorial)
+            return position;
+        
+        if (spectator.roleManager.CurrentRole is not SpectatorRole spectatorRole)
+            return position;
+        
+        if (spectatorRole.SyncedSpectatedNetId != tutorial.netId && spectatorRole.SyncedSpectatedNetId != 0)
+            return position;
+
+        return Vector3.zero;
+    }
+}
