d1ba6160b70a16be56d1e84659bb2c08f2478b59
jesusqc <jesusquirocampos@gmail.com>
2024-01-19 01:52:48 +0100

    fixes

--- HiddenTutorials/EntryPoint.cs
+++ HiddenTutorials/EntryPoint.cs
@@ -1,26 +1,19 @@
-﻿using System.Collections.Generic;
-using HarmonyLib;
-using MEC;
-using Mirror;
+﻿using HarmonyLib;
 using PlayerRoles;
-using PlayerRoles.FirstPersonControl;
 using PluginAPI.Core;
 using PluginAPI.Core.Attributes;
-using PluginAPI.Enums;
 using PluginAPI.Events;
-using RelativePositioning;
-using UnityEngine;
 
 namespace HiddenTutorials;
 
 // Thanks to xNexusACS for helping to test!
 public class EntryPoint
 {
-	public const string Version = "1.0.0.2";
+	public const string Version = "1.0.0.3";
 	
 	[PluginAPI.Core.Attributes.PluginConfig] public static PluginConfig Config;
 
-	private Harmony _harmony = new Harmony("com.jesusqc.hiddentutorials");
+	private readonly Harmony _harmony = new ("com.jesusqc.hiddentutorials");
 	
 	[PluginEntryPoint("HiddenTutorials", Version, "Makes tutorials hidden to spectators", "Jesus-QC")]
 	private void Init()
@@ -28,61 +21,8 @@ public class EntryPoint
 		if (!Config.IsEnabled)
 			return;
         
-		Log.Raw($"<color=blue>Loading HiddenTutorials {Version} by Jesus-QC</color>");
-		EventManager.RegisterEvents(this);
-		
 		_harmony.PatchAll();
-	}
-	
-	[PluginEvent(ServerEventType.PlayerChangeSpectator)]
-	private void OnPlayerChangeSpectator(PlayerChangeSpectatorEvent ev)
-	{
-		if (IsWhitelisted(ev.Player.ReferenceHub))
-			return;
-		
-		if (ev.OldTarget?.Role is RoleTypeId.Tutorial)
-			ResyncSpectator(ev.Player, ev.OldTarget);
-		
-		if (ev.NewTarget?.Role is not RoleTypeId.Tutorial)
-			return;
-
-		Timing.RunCoroutine(DesyncSpectator(ev.Player, ev.NewTarget));
-	}
-	
-	// Contains code from:
-	// -----------------------------------------------------------------------
-	// <copyright file="MirrorExtensions.cs" company="Exiled Team">
-	// Copyright (c) Exiled Team. All rights reserved.
-	// Licensed under the CC BY-SA 3.0 license.
-	// </copyright>
-	// -----------------------------------------------------------------------
-	private static void SendRoleAndPosition(Player spectator, Player target, RoleTypeId role, Vector3 position)
-	{
-		NetworkWriterPooled writer = NetworkWriterPool.Get();
-		writer.WriteUShort(38952);
-		writer.WriteUInt(target.NetworkId);
-		writer.WriteRoleType(role);
-
-		if (target.RoleBase is not IFpcRole fpc)
-			return;
-		
-		fpc.FpcModule.MouseLook.GetSyncValues(0, out ushort syncH, out _);
-		writer.WriteRelativePosition(new RelativePosition(position));
-		writer.WriteUShort(syncH);
-		
-		spectator.Connection.Send(writer.ToArraySegment());
-		NetworkWriterPool.Return(writer);
-	}
-	
-	private static void ResyncSpectator(Player spectator, Player target)
-	{
-		SendRoleAndPosition(spectator, target, target.Role, Vector3.zero);
-	}
-
-	private static IEnumerator<float> DesyncSpectator(Player spectator, Player target)
-	{
-		yield return Timing.WaitForOneFrame;
-		SendRoleAndPosition(spectator, target, RoleTypeId.Spectator, Vector3.zero);
+		Log.Raw($"<color=blue>Loading HiddenTutorials {Version} by Jesus-QC</color>");
 	}
 
 	public static bool IsWhitelisted(ReferenceHub referenceHub) => referenceHub.authManager.RemoteAdminGlobalAccess || referenceHub.serverRoles.RemoteAdmin || referenceHub.roleManager.CurrentRole.RoleTypeId is RoleTypeId.Overwatch;
---
+++ HiddenTutorials/Patches/VoiceChatPatch.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using HarmonyLib;
+using NorthwoodLib.Pools;
+using PlayerRoles;
+using PlayerRoles.Spectating;
+using PlayerRoles.Voice;
+
+namespace HiddenTutorials.Patches;
+
+[HarmonyPatch(typeof(SpectatorVoiceModule), nameof(SpectatorVoiceModule.ValidateReceive))]
+public class VoiceChatPatch
+{
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+    {
+        List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
+
+        Label skip = generator.DefineLabel();
+
+        newInstructions[0].labels.Add(skip);
+        
+        newInstructions.InsertRange(0, new CodeInstruction[]
+        {
+            new (OpCodes.Ldarg_0),
+            new (OpCodes.Ldarg_1),
+            new (OpCodes.Call, AccessTools.Method(typeof(VoiceChatPatch), nameof(CheckProximity))),
+            new (OpCodes.Brfalse_S, skip),
+            new (OpCodes.Ldc_I4_0),
+            new (OpCodes.Ret),
+        });
+        
+        foreach (CodeInstruction instruction in newInstructions)
+            yield return instruction;
+
+        ListPool<CodeInstruction>.Shared.Return(newInstructions);
+    }
+    
+    private static bool CheckProximity(VoiceModuleBase module, ReferenceHub speaker)
+    {
+        if (speaker.roleManager.CurrentRole.RoleTypeId != RoleTypeId.Tutorial)
+            return false;
+        
+        if (EntryPoint.IsWhitelisted(module.Owner))
+            return false;
+
+        if (module.Role is not SpectatorRole spectatorRole)
+            return false;
+        
+        return spectatorRole.SyncedSpectatedNetId == speaker.netId;
+    }
+}
